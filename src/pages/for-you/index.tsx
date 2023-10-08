452af92a95b26952d3ec23633ba019d909194783
glf30 <greyson.frazier@gmail.com>
2023-10-08 19:30:31 -0400

    Top of for you and book card components complete (design)

--- src/pages/for-you/index.tsx
+++ src/pages/for-you/index.tsx
@@ -10,18 +10,24 @@ import searchIcon from "public/assets/search-icon.svg";
 import settingsIcon from "public/assets/settings-wheel.svg";
 import questionIcon from "public/assets/question-icon.svg";
 import logoutIcon from "public/assets/logout-icon.svg";
-import hamburgerIcon from "public/assets/hamburger-menu.svg"
+import hamburgerIcon from "public/assets/hamburger-menu.svg";
+import testImg from "public/assets/test-img.png";
+import playButton from "public/assets/play-button.svg";
+import clockIcon from "public/assets/clock-icon.svg";
+import starIcon from "public/assets/star-icon.svg";
 
 const ForYou = () => {
   return (
     <>
-    <SideBar />
-    <div className="relative flex flex-column w-full md:ml-[200px] md:w-[calc(100%-200px)]">
-      
-   
+      <SideBar />
+      <div className="relative flex w-full flex-col md:ml-[200px] md:w-[calc(100%-200px)]">
         <SearchArea />
-        {/* <div>Main</div> */}
-    </div>
+        <div className="mx-auto my-0 w-full max-w-[1070px] px-6 py-0">
+          <div className="w-full px-0 py-10">
+            <ForYouArea />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
@@ -30,7 +36,7 @@ export default ForYou;
 
 const SideBar = () => {
   return (
-    <div className="hidden md:block fixed z-50 h-screen w-[200px] min-w-[200px] bg-[#f7faf9]">
+    <div className="fixed z-50 hidden h-screen w-[200px] min-w-[200px] bg-[#f7faf9] md:block">
       <div className="max-width-[160px] mx-auto my-0 flex h-14 items-center justify-center pt-4">
         <Image src={logo} alt="logo" className="h-10 w-40" />
       </div>
@@ -138,26 +144,124 @@ const SideBar = () => {
 
 const SearchArea = () => {
   return (
-    <div className="z-[1] h-20 border-b border-b-[#e1e7ea] bg-white w-full">
+    <div className="z-[1] h-20 w-full border-b border-b-[#e1e7ea] bg-white">
       <div className="relative mx-auto my-0 flex h-full max-w-[1070px] items-center justify-between px-8 py-0">
         {/* space holder */}
         <figure>
-          <Image src={""} height={0} width={0} alt="logo"  />
+          <Image src={""} height={0} width={0} alt="logo" />
         </figure>
-        <div className="flex items-center gap-6 max-w-[340px] w-full">
-          <div className="flex items-center w-full">
-            <div className="relative gap-2 flex items-center w-full">
-              <input type="text" className="h-10 w-full py-0 px-4 outline-none bg-[#f1f6f4] text-[#042330] border-2 border-[#e1e7ea] rounded-lg" placeholder="Search for books" autoComplete="off" />
-              <div className="flex items-center absolute h-full right-2 justify-end border-l-2 border-l-[#e1e7ea] pl-2">
+        <div className="flex w-full max-w-[340px] items-center gap-6">
+          <div className="flex w-full items-center">
+            <div className="relative flex w-full items-center gap-2">
+              <input
+                type="text"
+                className="h-10 w-full rounded-lg border-2 border-[#e1e7ea] bg-[#f1f6f4] px-4 py-0 text-[#042330] outline-none"
+                placeholder="Search for books"
+                autoComplete="off"
+              />
+              <div className="absolute right-2 flex h-full items-center justify-end border-l-2 border-l-[#e1e7ea] pl-2">
                 <Image src={searchIcon} alt="search icon" />
               </div>
             </div>
           </div>
-          <div className="flex md:hidden items-center justify-center cursor-pointer">
+          <div className="flex cursor-pointer items-center justify-center md:hidden">
             <Image src={hamburgerIcon} alt="home icon" height={24} width={24} />
           </div>
         </div>
       </div>
     </div>
+  );
+};
+
+const ForYouArea = () => {
+  return (
+    <div>
+      {/* For You Secition */}
+      <div className="mb-4 text-xl font-bold text-[#032b41]">
+        Selected just for you
+      </div>
+      <Link
+        className="mb-6 flex w-[calc((100%/3)*2)] flex-col justify-between gap-6 rounded-[4px] bg-[#fbefd5] p-6 md:flex-row"
+        href={`/book`}
+      >
+        <div className="w-full text-[#032b41] md:w-[40%] ">
+          How Constant Innovation Creates Radically Successful Businesses
+        </div>
+        <div className="w-[1px] bg-[#bac8ce]"></div>
+        <div className="flex w-3/5 gap-4">
+          <figure className="h-[140px] w-[140px] min-w-[140px]">
+            <Image src={testImg} alt="" />
+          </figure>
+          <div className="w-full">
+            <div className="mb-2 font-semibold text-[#032b41]">
+              The Lean Startup
+            </div>
+            <div className="mb-4 text-sm text-[#394547]">Eric Riles</div>
+            <div className="flex items-center gap-2">
+              <div className="min-w-10 flex h-10 w-10 items-center">
+                <Image src={playButton} alt="" width={40} height={40} />
+              </div>
+              <div className="text-sm font-medium text-[#032b41]">
+                3 mins 23 secs
+              </div>
+            </div>
+          </div>
+        </div>
+      </Link>
+      {/* Recommended Section */}
+      <div>
+        <div className="mb-4 text-xl font-bold text-[#032b41]">
+          Recommended For You
+        </div>
+        <div className="mb-4 font-light text-[#394547]">
+          We think you'll like these
+        </div>
+        {/* Books Container */}
+        <div className="mb-32 flex gap-4 overflow-x-clip">
+          {
+            Array(5).fill(0).map(() => (
+              <BookCard />
+            ))
+          }
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const BookCard = () => {
+  return (
+    <Link
+      href={"/for-you"}
+      className="relative w-full max-w-[200px] rounded px-3 pb-3 pt-8 duration-200 hover:bg-[#f1f6f4]"
+    >
+      <div className="absolute right-0 top-0 flex h-[18px] w-fit items-center rounded-[20px] bg-[#032b41] px-2 py-0 text-[10px] text-white">
+        Premium
+      </div>
+      <figure className="h-[172px] w-[172px]">
+        <Image src={testImg} alt="" />
+      </figure>
+      <div className="mb-2 text-base font-bold text-[#032b41]">Mastery</div>
+      <div className="mb-2 text-sm font-light text-[#6b757b]">
+        Dale Dalington
+      </div>
+      <div className="mb-2 text-sm text-[#394547]">
+        Time tested advice for the digital age
+      </div>
+      <div className="flex gap-2">
+        <div className="flex items-center gap-1 text-sm font-light text-[#6b757b]">
+          <div className="flex h-4 w-4">
+            <Image src={clockIcon} alt="" />
+          </div>
+          <div className="text-sm font-light text-[#6b757b]">0:34</div>
+        </div>
+        <div className="flex items-center gap-1 text-sm font-light text-[#6b757b]">
+          <div className="flex h-4 w-4">
+            <Image src={starIcon} alt="" />
+          </div>
+          <div className="text-sm font-light text-[#6b757b]">4.4</div>
+        </div>
+      </div>
+    </Link>
   );
 };
